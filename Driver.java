784d814f2a252008f722d4169fda3bcac207c4fc
andreawang01 <andreawang502@gmail.com>
2024-11-03 20:18:07 -0800

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,18 @@
+public class Driver {
+    public static void main (String[] args) {
+        //activity 1 question 2//
+        System.out.println(Review.sentimentVal("happily"));  
+        System.out.println(Review.sentimentVal("terrible"));
+        System.out.println(Review.sentimentVal("cold"));
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+}
